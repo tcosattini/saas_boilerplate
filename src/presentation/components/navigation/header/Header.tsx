e0ea434fec0f22451d2bcac9bb9b9fd92c8b41ce
Thibault COSATTINI <t.cosattini@gmail.com>
2023-09-27 22:18:15 +0200

    profile dropdown WIP

--- src/presentation/components/navigation/header/Header.tsx
+++ src/presentation/components/navigation/header/Header.tsx
@@ -1,8 +1,12 @@
+import ProfileDropdown from "../../profile/ProfileDropdown";
+
 export default function Header() {
   return (
     <div className="flex flex-grow z-10 h-16 border-b border-gray-200  flex-shrink-0 bg-white">
-      <div className="flex flex-1 justify-between px-4">
-        <div className="ml-4 flex items-center"></div>
+      <div className="flex flex-1 justify-end px-4">
+        <div className="flex items-center justify-end">
+          <ProfileDropdown />
+        </div>
       </div>
     </div>
   );
