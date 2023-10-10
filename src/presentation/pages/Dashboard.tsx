625f3214ad108b0a9d71ca98aae9e1011d91ebc5
tcosattini <76822619+tcosattini@users.noreply.github.com>
2023-10-10 21:14:41 +0200

    feat / modal WIP (#6)
    * feat / modal WIP
    
    * modal with transitions

--- src/presentation/pages/Dashboard.tsx
+++ src/presentation/pages/Dashboard.tsx
@@ -1,8 +1,16 @@
+import { useState } from "react";
+import Modal from "../components/modal/Modal";
 import Header from "../components/navigation/header/Header";
 import SideNavbar from "../components/navigation/navbar/sideNavbar/SideNavbar";
 import { NavbarContextProvider } from "../contexts/navigation/navbar/NavbarContext";
 
 export const Dashboard = () => {
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+
+  const handleClick = () => {
+    setIsModalOpen(!isModalOpen);
+  };
+
   return (
     <>
       <div className="flex">
