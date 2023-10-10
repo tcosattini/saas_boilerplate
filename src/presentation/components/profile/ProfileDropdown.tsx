7d6300480abfa7104eb6479eb0c70ca78d5bb646
Thibault COSATTINI <t.cosattini@gmail.com>
2023-10-10 21:13:39 +0200

    modal with transitions

--- src/presentation/components/profile/ProfileDropdown.tsx
+++ src/presentation/components/profile/ProfileDropdown.tsx
@@ -34,7 +34,7 @@ export default function ProfileDropdown() {
           )}
         </button>
       </div>
-      <Dialog open={isDropDownOpen} onClose={() => setIsDropDownOpen(false)}>
+      <Dialog open={isDropDownOpen} onClose={setIsDropDownOpen}>
         <Dialog.Panel
           className="absolute right-4 top-14 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
           role="menu"
--- src/routes/routes.const.ts
+++ src/routes/routes.const.ts
@@ -1,1 +1,2 @@
 export const ROUTE_HOME = "/";
+export const ROUTE_TEST_MODAL = "/test_modal";
