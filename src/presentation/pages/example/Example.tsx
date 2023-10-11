0e3344b5d674c94abe37c37852d3838b0fddf47a
Thibault COSATTINI <t.cosattini@gmail.com>
2023-10-11 11:08:05 +0200

    content management

--- src/presentation/pages/example/Example.tsx
+++ src/presentation/pages/example/Example.tsx
@@ -1,10 +1,7 @@
 export default function Example() {
   return (
-    <div className="py-2">
-      <div className="mx-auto max-w-7xl px-2 sm:px-6 md:px-8">
-        <h1 className="text-2xl font-semibold text-gray-900">Example 1</h1>
-      </div>
-      <div className="mx-auto max-w-7xl px-4  md:px-8">
+    <div className="py-8">
+      <div className="mx-auto max-w-7xl px-4 md:px-8">
         <div className="py-1">
           <div className="h-screen rounded-lg border-4 border border-gray-200">
             <div
