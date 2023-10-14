dbf9fce281e382a2f95ae2c1506d7987e1d31297
tcosattini <76822619+tcosattini@users.noreply.github.com>
2023-10-14 10:50:27 +0200

    Feature/no ref/slide overs (#10)
    * feat / slide-overs
    
    * feat / slide-overs

--- src/presentation/pages/example/Example.tsx
+++ src/presentation/pages/example/Example.tsx
@@ -1,14 +1,10 @@
-import SimpleTable from "../../components/lists/tables/simple-table/SimpleTable";
-import SimpleSlideOver from "../../components/overlays/slide-overs/simple-slide-over/SimpleSlideOver";
+
 import DashboardWrapper from "../dashboard-wrapper/DashboardWrapper";
 
 export default function ExamplePage() {
   return (
     <>
-      <DashboardWrapper>
-        <SimpleTable />
-        <SimpleSlideOver />
-      </DashboardWrapper>
+      <DashboardWrapper>{/* your components */}</DashboardWrapper>
     </>
   );
 }
